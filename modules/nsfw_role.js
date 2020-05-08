3ff8d7f441b0d251a39d8c70aa3c950d91511b57
underscore11code <minecrafter11mrt@gmail.com>
2020-05-08 03:55:23 -0700

    Hopefully works

---
+++ modules/nsfw_role.js
@@ -0,0 +1,10 @@
+module.exports = function (client) {
+    client.on('messageReactionAdd', (reaction, user) => {
+        let data = require('../data.json')
+        if (reaction.message.id !==  data.nsfw_message) return;
+        if (!reaction.message.guild) return;
+        if (reaction.member.roles.has(data.nsfw_role)) reaction.member.removeRole(data.nsfw_role)
+        else reaction.member.addRole(data.nsfw_role)
+        reaction.remove()
+    })
+}
