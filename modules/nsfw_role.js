eaa7c5efea49f15784c00e06f15b2eb3ba2ab2a5
underscore11code <minecrafter11mrt@gmail.com>
2020-05-08 04:06:33 -0700

    Remove debug calls, add await

--- modules/nsfw_role.js
+++ modules/nsfw_role.js
@@ -1,14 +1,11 @@
 module.exports = function (client) {
-    client.on('messageReactionAdd', (reaction, user) => {
+    client.on('messageReactionAdd', async (reaction, user) => {
         let data = require('../data.json')
         if (reaction.message.id !==  data.nsfw_message) return;
-        console.log('Is message')
         if (!reaction.message.guild) return;
-        console.log('Is in guild')
         if (reaction.emoji !== '✅') return;
-        console.log('Is right emoji, toggling')
-        if (reaction.member.roles.has(data.nsfw_role)) reaction.member.removeRole(data.nsfw_role)
-        else reaction.member.addRole(data.nsfw_role)
+        if (reaction.member.roles.has(data.nsfw_role)) await reaction.member.removeRole(data.nsfw_role)
+        else await reaction.member.addRole(data.nsfw_role)
         reaction.remove()
     })
 }
