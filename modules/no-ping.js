e0bfb92ab442e3bcd5b0bc03617763f55e0326a6
Underscore11 <minecrafter11mrt@gmail.com>
2020-05-08 02:29:08 -0700

    fix everything

--- modules/no-ping.js
+++ modules/no-ping.js
@@ -1,5 +1,4 @@
 const data = require('../data.json');
-
 module.exports = function (client) {
   client.on('message', async msg => {
     // Ignore DMs and messages that don't mention anyone
@@ -7,7 +6,16 @@ module.exports = function (client) {
     if (msg.mentions.members.size === 0) return;
     if (msg.author.id === client.user.id) return;
 
-    if (/<(@|@&|#)\d+>/g.test(msg.content)) {
+//     const senderIsStaff = msg.member.roles.some(role => data.staff_roles.indexOf(role.name) !== -1);
+//     if (senderIsStaff) {
+//       return;
+//     }
+    const mentionsStaff = msg.mentions.members.some(member => {
+      // If the message mentions any members that satisfy the following:
+      return member.roles.some(role => data.staff_roles.indexOf(role.name) !== -1);
+    });
+    if (mentionsStaff) {
+      // Tell them off:
       await msg.channel.send(`Hey ${msg.author}! Please don't tag people directly.`);
     }
   });
