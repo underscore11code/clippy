29efbafc7687cc04be3f79b2c5bca980deaa7a23
Larry <larrydblomme@hotmail.com>
2020-05-08 01:29:00 -0600

    give everyone staff

--- modules/role-on-join.js
+++ modules/role-on-join.js
@@ -1,7 +1,7 @@
 const data = require('../data.json');
 
 module.exports = (client) => {
-  client.on('guildMemberAdd', async (member) => {
+  client.on('guildMemberAdd', async (Staff) => {
     await member.addRole(data.member_role, 'Autorole');
   });
 };
