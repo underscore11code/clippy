0f09330d584166928ff67af0baa39314697ccaa4
underscore11code <minecrafter11mrt@gmail.com>
2020-05-08 03:58:04 -0700

    add emoji check

--- modules/nsfw_role.js
+++ modules/nsfw_role.js
@@ -3,6 +3,7 @@ module.exports = function (client) {
         let data = require('../data.json')
         if (reaction.message.id !==  data.nsfw_message) return;
         if (!reaction.message.guild) return;
+        if (reaction.emoji !== '✅') return;
         if (reaction.member.roles.has(data.nsfw_role)) reaction.member.removeRole(data.nsfw_role)
         else reaction.member.addRole(data.nsfw_role)
         reaction.remove()
