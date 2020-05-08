0915d8ce58d53d56b26f8bd45f8e4e2f1bd33335
Underscore11 <minecrafter11mrt@gmail.com>
2020-05-08 01:51:03 -0700

    no mentions for anyone

--- modules/no-ping.js
+++ modules/no-ping.js
@@ -7,19 +7,9 @@ module.exports = function (client) {
     if (msg.mentions.members.size === 0) return;
     if (msg.author.id === client.user.id) return;
 
-//     const senderIsStaff = msg.member.roles.some(role => data.staff_roles.indexOf(role.name) !== -1);
-//     if (senderIsStaff) {
-//       return;
-//     }
-
-    const mentionsStaff = msg.mentions.members.some(member => {
-      // If the message mentions any members that satisfy the following:
-      return member.roles.some(role => data.staff_roles.indexOf(role.name) !== -1);
-    });
-
-    if (mentionsStaff) {
+    if (/<(@|@&|#)\d+>/.test(msg.content)) {
       // Tell them off:
-      await msg.channel.send(`Hey ${msg.author}! Please don't tag people directly.`);
+      await msg.channel.send(`Hey ${msg.author}! Please don't tagdirectly.`);
     }
   });
 };
