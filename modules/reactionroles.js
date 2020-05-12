e986321793a7a7eaff1d2bffc14f9cf0b25a646e
underscore11code <minecrafter11mrt@gmail.com>
2020-05-11 23:22:37 -0700

    add reaction roles

---
+++ modules/reactionroles.js
@@ -0,0 +1,38 @@
+const data = require('../data.json');
+const RichEmbed = require('discord.js').RichEmbed;
+module.exports = client => {
+    let reactionCallback = async (reaction, user) => {
+        if (user.equals(user.client.user)) return;
+        let rr = data.reactionroles.find(rr => rr.emoji === reaction.emoji.name);
+        if (rr === undefined) return reaction.remove(user);
+        if (!reaction.message.guild) return;
+        let member = reaction.message.guild.members.get(user.id);
+        let message = '';
+        if (member.roles.find(role => role.id === rr.id))
+            await member.removeRole(rr.id);
+        else
+            await member.addRole(rr.id);
+        reaction.remove(user);
+    }
+
+    client.on('ready', async () => {
+        let channel = client.channels.get(data.reactionroleschannel);
+        await channel.bulkDelete(100);
+        let embed = new RichEmbed();
+        embed.setColor('#94df03')
+            .setTitle('Claimable Roles')
+            .setDescription('Click the reaction to get the role! Click it again to remove it!');
+        data.reactionroles.forEach(rr => {
+            embed.addField(`${rr.emoji} ${channel.guild.roles.get(rr.id).name}`, rr.description, true);
+        })
+        let message = await channel.send(embed);
+        data.reactionroles.forEach(rr => message.react(rr.emoji));
+        client.on('messageReactionAdd', reactionCallback)
+    })
+
+    client.on('message', async message => {
+        if (message.channel.id !== data.reactionroleschannel) return;
+        if (message.author.id === message.client.user.id) return;
+        message.delete();
+    });
+}
